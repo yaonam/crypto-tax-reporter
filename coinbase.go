30b3276da69e030fc28d331d1504dd3a0fa897cc
Elim <elimviolinist@gmail.com>
2023-03-17 22:50:57 -0400

    Coinbase type handling

--- coinbase.go
+++ coinbase.go
@@ -53,13 +53,16 @@ func parseTxList(userID uint, data [][]string) []Transaction {
 	var txList []Transaction
 	for i, line := range data {
 		if i > 0 { // skip headers
+			// Handle based on type
+			// select case...
+
 			// Coinbase columns
 			var tx Transaction
 			tx.Timestamp = line[0]
 			tx.Type = line[1]
-			tx.Asset = findAssetOrCreate(line[2]) // Need to conver
+			tx.Asset = findAssetOrCreate(line[2])
 			tx.Quantity = parseFloatOrZero(line[3])
-			tx.Currency = findAssetOrCreate(line[4]) // Need to conver
+			tx.Currency = findAssetOrCreate(line[4])
 			tx.SpotPrice = parseFloatOrZero(line[5])
 			tx.Subtotal = parseFloatOrZero(line[6])
 			tx.Total = parseFloatOrZero(line[7])
@@ -79,3 +82,47 @@ func parseTxList(userID uint, data [][]string) []Transaction {
 	}
 	return txList
 }
+
+// TODO: Figure out if I need to make args pointers
+func handleConvert(userID uint, txList []Transaction, line []string) {
+	notesSplit := strings.Split(line[9], " ")
+
+	// Create sell sellTx
+	var sellTx Transaction
+	sellTx.Timestamp = line[0]
+	sellTx.Type = line[1]
+	sellTx.Asset = findAssetOrCreate(notesSplit[2])
+	sellTx.Quantity = parseFloatOrZero(notesSplit[1])
+	sellTx.Currency = findAssetOrCreate(line[4])
+	sellTx.SpotPrice = parseFloatOrZero(line[5])
+	// Total will be less than subtotal due to fees
+	sellTx.Subtotal = parseFloatOrZero(line[7])
+	sellTx.Total = parseFloatOrZero(line[6])
+	sellTx.Fees = parseFloatOrZero(line[8])
+	sellTx.Notes = line[9]
+
+	// Accounts
+	sellTx.From = userID
+
+	txList = append(txList, sellTx)
+
+	// Create buy tx
+	var buyTx Transaction
+	buyTx.Timestamp = line[0]
+	buyTx.Type = line[1]
+	buyTx.Asset = findAssetOrCreate(notesSplit[5])
+	buyTx.Quantity = parseFloatOrZero(notesSplit[4])
+	buyTx.Currency = findAssetOrCreate(line[4])
+	buyTx.SpotPrice = parseFloatOrZero(line[5])
+	buyTx.Subtotal = parseFloatOrZero(line[6])
+	buyTx.Total = parseFloatOrZero(line[6])
+	buyTx.Fees = 0
+	buyTx.Notes = line[9]
+
+	// Accounts
+	buyTx.From = userID
+
+	txList = append(txList, buyTx)
+}
+
+func handleReward() {}
